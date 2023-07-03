1b0b0e4a4832114e5090c6f6e2b1e9e05a8a4466
JakeSaintG <jakestgermain@outlook.com>
2023-07-02 23:54:22 -0400

    Update sln - New .Data project - Set up initial DB using EF and Sqlite

---
+++ PokeDex/RazorPokedex.Data/Context.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPokedex.Data;
+
+public class Context : DbContext
+{
+    public DbSet<PokeDexEntry> PokeDexEntries { get; set; }
+
+    public Context() : base()
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=../RazorPokedex.Data/Pokedex.db;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PokeDexEntry>().ToTable("PokeDexEntries");
+    }
+}
--- PokeDex/RazorPokedex/Program.cs
+++ PokeDex/RazorPokedex/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using RazorPokedex.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<Context>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
