119426616f340461a303385aaed4e07f0fb57c4e
JakeSaintG <jakestgermain@outlook.com>
2023-07-04 00:03:38 -0400

    Pokedex Log - Remove form to add pokemon - Start styling

--- PokeDex/RazorPokedex/Pages/Log.cshtml.cs
+++ PokeDex/RazorPokedex/Pages/Log.cshtml.cs
@@ -10,9 +10,6 @@ public class LogModel : PageModel
     private readonly Context _context;
     public List<PokeDexEntry> DexEntries { get; set; } = new List<PokeDexEntry>();
 
-    [BindProperty]
-    public PokeDexEntry NewEntry { get; set; }
-
     public LogModel(Context context)
     {
         _context = context;
@@ -21,21 +18,5 @@ public class LogModel : PageModel
     public void OnGet()
     {
         DexEntries = _context.PokeDexEntries.ToList();
-    }
-
-    //IActionResult allows Razor to redirect to a page at the end of execution
-    public IActionResult OnPost() 
-    {
-        //TODO: Investigate if SQLite will let me use GUIDs/UUIDs
-        NewEntry.Id = Guid.NewGuid().ToString();
-        NewEntry.IsHomebrew = true;
-        
-        //TODO: Placeholder; hardcoded for now
-        NewEntry.ImageURL = "https://www.serebii.net/pokearth/sprites/rb/000.png";
-
-        _context.PokeDexEntries.Add(NewEntry);
-        _context.SaveChanges();
-
-        return RedirectToPage();
     }
 }
