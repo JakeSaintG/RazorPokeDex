17827a6fd5c0ed698655f64b56e7fd74f314bf9e
Jake St. Germain <jakesaint27@gmail.com>
2023-07-19 22:18:44 -0400

    Finish up AddOrEditPkmn

--- PokeDex/RazorPokedex/Pages/AddOrEditPkmn.cshtml.cs
+++ PokeDex/RazorPokedex/Pages/AddOrEditPkmn.cshtml.cs
@@ -8,6 +8,7 @@ namespace RazorPokedex.Pages;
 public class AddOrEditPkmnModel : PageModel
 {
 
+    public string PageHeader { get; set; } = "Add to PokéDex.";
     private readonly Context _context;
 
     [BindProperty]
@@ -22,22 +23,52 @@ public class AddOrEditPkmnModel : PageModel
     {
         if (id != null)
         {
-            Console.WriteLine("");
             AddOrEditEntry = _context.PokeDexEntries.FromSql($"SELECT * FROM PokeDexEntries WHERE Id = {id} LIMIT 1").Single();
+            PageHeader = $"Edit Dex Entry:{AddOrEditEntry.Name}";
         }
     }
 
     //IActionResult allows Razor to redirect to a page at the end of execution
     public IActionResult OnPost()
     {
-        //TODO: Investigate if SQLite will let me use GUIDs/UUIDs
-        AddOrEditEntry.Id = Guid.NewGuid().ToString();
-        AddOrEditEntry.IsHomebrew = true;
 
-        //TODO: Placeholder; hardcoded for now
-        AddOrEditEntry.ImageURL = "https://www.serebii.net/pokearth/sprites/rb/000.png";
+        if (AddOrEditEntry.Id == null)
+        {
+            AddOrEditEntry.Id = Guid.NewGuid().ToString();
+            AddOrEditEntry.IsHomebrew = true;
 
-        _context.PokeDexEntries.Add(AddOrEditEntry);
+            //TODO: Placeholder; hardcoded for now
+            AddOrEditEntry.ImageURL = "https://www.serebii.net/pokearth/sprites/rb/000.png";
+
+            _context.PokeDexEntries.Add(AddOrEditEntry);
+        }
+        else
+        {
+            var editedEntry = _context.PokeDexEntries.FromSql($"SELECT * FROM PokeDexEntries WHERE Id = {AddOrEditEntry.Id} LIMIT 1").Single();
+
+            //TODO: Temporary. Merge these objs.
+            if (editedEntry.Name != AddOrEditEntry.Name)
+                editedEntry.Name = AddOrEditEntry.Name;
+
+            if (editedEntry.Type1 != AddOrEditEntry.Type1)
+                editedEntry.Type1 = AddOrEditEntry.Type1;
+
+            if (editedEntry.Type2 != AddOrEditEntry.Type2)
+                editedEntry.Type2 = AddOrEditEntry.Type2;
+
+            if (editedEntry.Height != AddOrEditEntry.Height)
+                editedEntry.Height = AddOrEditEntry.Height;
+
+            if (editedEntry.Weight != AddOrEditEntry.Weight)
+                editedEntry.Weight = AddOrEditEntry.Weight;
+
+            if (editedEntry.Category != AddOrEditEntry.Category)
+                editedEntry.Category = AddOrEditEntry.Category;
+
+            if (editedEntry.FlavorTXT != AddOrEditEntry.FlavorTXT)
+                editedEntry.FlavorTXT = AddOrEditEntry.FlavorTXT;
+        }
+
         _context.SaveChanges();
 
         return RedirectToPage("./Log");
